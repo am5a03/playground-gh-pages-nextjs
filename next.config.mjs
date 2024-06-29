5a0ddeb395310516266d44df289131fab33b9a03
Raymond Chan <am5a03@gmail.com>
2024-06-28 18:32:38 -0700

    deploy: attempt to fix deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,44 +1,32 @@
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
+const devNextConfig = {};
+const prodNextConfig = {
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/playground-gh-pages-nextjs",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+};
 
 /** @type {import('next').NextConfig} */
-const nextConfig = (phase) => {
-  const commonConfig = {};
-  const devConfig = {};
-  const prodConfig = {
-    /**
-     * Enable static exports for the App Router.
-     *
-     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-     */
-    output: "export",
-
-    /**
-     * Set base path. This is the slug of your GitHub repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    basePath: "/playground-gh-pages-nextjs",
-
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-     */
-    images: {
-      unoptimized: true,
-    },
-  };
-
-  return phase === PHASE_DEVELOPMENT_SERVER
-    ? {
-        ...commonConfig,
-        ...devConfig,
-      }
-    : {
-        ...commonConfig,
-        ...prodConfig,
-      };
-};
+const nextConfig =
+  process.env.NODE_ENV === "production" ? prodNextConfig : devNextConfig;
 
 export default nextConfig;
